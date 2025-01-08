8a4e302eee84c125fbaf61d5cba32b0a8f5188e0
Iam4lex <alexmwangimungai254@gmail.com>
2025-01-08 12:50:05 +0300

    Pytton classes

--- Python classes/classes.py
+++ Python classes/classes.py
@@ -1,2 +1,17 @@
 
 # classes in python
+
+class Person:
+
+    def __init__(self, name, age, height):
+        self.name = name
+        self.age = age
+        self.height = height
+
+    def greet(self):
+        print(f"Hello {self.name}, you're {self.age} years old and {self.height} feet high.")
+
+person1 = Person('Alex', 20, 5)
+person1.greet()
+
+print(person1.name)
